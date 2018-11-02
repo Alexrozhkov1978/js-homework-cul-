fbdfeb7eb28690798be4161e7c5f8b17664599af
Oleksandr Rozhkov <alexshura@gmail.com>
2018-11-02 09:24:37 +0000

    Some chenges

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-const showExtention = document.querySelector('h4');
+const showExtention = document.querySelector('h4'); // введений користувачем вираз, "перезаписує" відповідний string html 
 const display = document.querySelector('.display');
-let str = display.value[0];
+let str = display.value[0]; // змінна потрібна для фукції butdel, яка видаляє останній введений символ. + неможливість видалення пустого значення.   
 
 document.querySelectorAll('.digits, .act') /*.adds*/
     .forEach(el => el.addEventListener('click', digitActClick));
@@ -14,19 +14,20 @@ document.querySelectorAll('.equal')
     .forEach(el => el.addEventListener('click', equalActClick));
     
 function equalActClick(e){
-    checkCrash(display.value);
     showExtention.textContent = display.value;
+    // вставлена перевірка чи значення може бути +- Infinity
     display.value = ((parseFloat(eval(display.value)) === parseFloat(eval(display.value))) && Math.abs(eval(display.value)) < Number.MAX_SAFE_INTEGER) ? eval(display.value) : 
     (alert ('This expression has no valid values'), '');
 } 
 
+// очистити поле вводу
 const butclear = document.getElementById('butclear');
     butclear.addEventListener('click', function (cl) {
     display.value = ""
     showExtention.textContent = '...';
 });
 
-
+// змінити значення на +-
 const butminus = document.getElementById('butminus');
     butminus.addEventListener('click', function (minus) {
     showExtention.textContent = (display.value !== '') ?( ' -1 * (' + (display.value) + ')' ): '...';
@@ -34,6 +35,7 @@ const butminus = document.getElementById('butminus');
     (alert ('This expression has no valid values'), '');
 });
 
+// видалити останній символ
 const butdel = document.getElementById('butdel');
     butdel.addEventListener('click', function (deleteLast) {
     str = (display.value.length<=1) ? (''):display.value[0];
@@ -44,13 +46,7 @@ const butdel = document.getElementById('butdel');
     display.value = str;
 });
 
-function checkCrash (ells) {
-    1-1;
-    
-    alert (display.value[parseInt(display.value.length-1)])
-    alert ((display.value[display.value.length-1] === '/'||'*'||'-'||'+'));
-    let p = display.value[display.value.length-1]
-    ells = (display.value[display.value.length-1] === '/'||'*'||'-'||'+') ? alert ('Please do something with last element!') : alert ('Boo!')
-    return ells
-}
-
+const butmemorylist = document.getElementById('butmemorylist');
+    butmemorylist.addEventListener('click', function (mem) {
+    document.querySelector('h5').textContent = showExtention.textContent + ' = ' + display.value;
+});
